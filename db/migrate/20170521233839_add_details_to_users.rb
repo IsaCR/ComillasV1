bce78674271f99d597115dbe7ce04fbdcf4c27c7
IsaCR <isagonzacr@gmail.com>
2017-05-21 17:47:36 -0600

    Add new columns to Users table
     - Create new migrations
    
    Command:
     -rails generate migration AddDetailsToUsers name:string
    apellido:string is_admin:boolean is_student:boolean

---
+++ db/migrate/20170521233839_add_details_to_users.rb
@@ -0,0 +1,8 @@
+class AddDetailsToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :name, :string
+    add_column :users, :apellido, :string
+    add_column :users, :is_admin, :boolean default: false
+    add_column :users, :is_student, :boolean default:false
+  end
+end
