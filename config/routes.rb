e0041e46333eca4286c459bbf408f53e03389772
Isabel <isagonzacr@gmail.com>
2017-06-27 21:24:35 -0600

    Implementing Template to Home

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  root 'projects#index'
   resources :user_skills
   resources :skills
   resources :portfolios
   resources :projects
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # root to: "home#index"
 end
