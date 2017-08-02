0d08d26bff922a06c898f34b726427c2da912610
Isabel <isagonzacr@gmail.com>
2017-08-02 15:34:54 -0600

    Adding images to users and projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,12 @@
 class Project < ApplicationRecord
 	belongs_to :user
 	has_and_belongs_to_many :skills
+
+	has_attached_file :image,
+										styles: { thumb: "300x300>",
+															medium: "700x400>" },
+										default_url: "/images/:style/missing.png"
+	validates_attachment_content_type :image,
+																		size: { :in => 0..10.megabytes },
+																		content_type: ["image/jpeg", "image/gif", "image/png","image/jpg"]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170705000422) do
+ActiveRecord::Schema.define(version: 20170802202137) do
 
   create_table "portfolios", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "title"
@@ -25,8 +25,18 @@ ActiveRecord::Schema.define(version: 20170705000422) do
     t.string   "description"
     t.boolean  "is_active"
     t.integer  "user_id"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+  end
+
+  create_table "projects_skills", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "project_id", null: false
+    t.integer "skill_id",   null: false
+    t.index ["project_id", "skill_id"], name: "index_projects_skills_on_project_id_and_skill_id", using: :btree
   end
 
   create_table "skills", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -35,30 +45,37 @@ ActiveRecord::Schema.define(version: 20170705000422) do
     t.datetime "updated_at",  null: false
   end
 
-  create_table "user_skills", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.integer  "user_id"
-    t.integer  "skill_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+  create_table "skills_users", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "user_id",  null: false
+    t.integer "skill_id", null: false
+    t.index ["user_id", "skill_id"], name: "index_skills_users_on_user_id_and_skill_id", using: :btree
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",          default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
     t.string   "name"
     t.string   "lastname"
-    t.boolean  "is_admin"
-    t.boolean  "is_student"
+    t.boolean  "is_admin",               default: false
+    t.boolean  "is_student",             default: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
