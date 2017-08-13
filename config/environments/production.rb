7822b13061f50461e33e70827f336972c0eab99d
Isabel <isagonzacr@gmail.com>
2017-08-13 17:32:53 -0600

    Config smtp variable production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,18 +81,15 @@ Rails.application.configure do
     config.logger = ActiveSupport::TaggedLogging.new(logger)
   end
 
-  config.action_mailer.default_url_options = { :host => 'comillasv1.heroku.com' }
-
-
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # config.action_mailer.default_url_options = {:host => 'yourdomain.com'}
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   :address => "127.0.0.1",
-  #   :port    => 25,
-  #   :domain  => 'yourdomain.com'
-  # }
+  config.action_mailer.default_url_options = { :host => 'comillasv1.heroku.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "127.0.0.1",
+    :port    => 25,
+    :domain  => 'comillasv1.heroku.com'
+  }
   # config.assets.compile = true
 end
